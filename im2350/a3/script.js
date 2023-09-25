5acb31587b6e8a3de462672f17bc6265ceeff484
Goulafre <126740071+Goulafre@users.noreply.github.com>
2023-09-25 15:42:32 +1000

    Add files via upload

---
+++ im2350/a3/script.js
@@ -0,0 +1,21 @@
+
+
+window.red = 0;
+window.green = 0;
+window.blue = 0;
+function func_1(value) {
+    window.red = value;
+    change_bg();
+}
+function func_2(value) {
+    window.green = value;
+    change_bg();
+}
+function func_3(value) {
+    window.blue = value;
+    change_bg();
+}
+function change_bg() {
+    document.getElementById("output").innerHTML = window.red+", "+window.green+", "+window.blue;
+    document.body.style.backgroundColor = "rgb("+window.red+","+window.green+","+window.blue+")";
+}
