de7a40cce3988f69c64e8c73a863287acd920bde
Goulafre <126740071+Goulafre@users.noreply.github.com>
2023-10-17 16:54:35 +1100

    Add files via upload

--- im2350/a3/script.js
+++ im2350/a3/script.js
@@ -1,21 +1,53 @@
 
 
-window.red = 0;
-window.green = 0;
-window.blue = 0;
-function func_1(value) {
-    window.red = value;
-    change_bg();
+function updateValue(e) {
+    console.log(e.target.value)
+
+    if (e.target.value > 70 && e.target.value <80) {
+      document.getElementById("poem1").style.opacity = '1'
+    } else {
+      document.getElementById("poem1").style.opacity = '0'
+    }
+    if (e.target.value > 20 && e.target.value <30) {
+        document.getElementById("poem3").style.opacity = '1'
+      } else {
+        document.getElementById("poem3").style.opacity = '0'
+      }
+      if (e.target.value > 20 && e.target.value <30) {
+        document.getElementById("poem5").style.opacity = '1'
+      } else {
+        document.getElementById("poem5").style.opacity = '0'
+      }
+  }
+
+  function updateValue2(e) {
+    console.log(e.target.value)
+    document.body.style.backgroundColor = "color-mix( in srgb, #C1fa5e " + e.target.value + "%, #34c9eb)";
+    
+    if (e.target.value > 70 && e.target.value <80) {
+      document.getElementById("poem2").style.opacity = '1'
+    } else {
+      document.getElementById("poem2").style.opacity = '0'
+    }
+    if (e.target.value > 20 && e.target.value <30) {
+        document.getElementById("poem4").style.opacity = '1'
+      } else {
+        document.getElementById("poem4").style.opacity = '0'
+      } 
+    if (e.target.value > 20 && e.target.value <30) {
+    document.getElementById("background-video").style.opacity = '.3'
+  } else {
+    document.getElementById("background-video").style.opacity = '0'
+  }
 }
-function func_2(value) {
-    window.green = value;
-    change_bg();
-}
-function func_3(value) {
-    window.blue = value;
-    change_bg();
-}
-function change_bg() {
-    document.getElementById("output").innerHTML = window.red+", "+window.green+", "+window.blue;
-    document.body.style.backgroundColor = "rgb("+window.red+","+window.green+","+window.blue+")";
-}
+  
+
+  
+
+  
+  document.getElementById("rangeInput").addEventListener("input", updateValue)
+
+  document.getElementById("rangeInput2").addEventListener("input", updateValue2)
+
+ 
+
