6e2624ba10462e4e2fe5662a8db26b3eff908ca7
Goulafre <126740071+Goulafre@users.noreply.github.com>
2023-10-17 17:52:17 +1100

    Add files via upload

--- im2350/a3/script.js
+++ im2350/a3/script.js
@@ -13,7 +13,7 @@ function updateValue(e) {
       } else {
         document.getElementById("poem3").style.opacity = '0'
       }
-      if (e.target.value > 20 && e.target.value <30) {
+      if (e.target.value > 45 && e.target.value <55) {
         document.getElementById("poem5").style.opacity = '1'
       } else {
         document.getElementById("poem5").style.opacity = '0'
@@ -24,20 +24,30 @@ function updateValue(e) {
     console.log(e.target.value)
     document.body.style.backgroundColor = "color-mix( in srgb, #C1fa5e " + e.target.value + "%, #34c9eb)";
     
-    if (e.target.value > 70 && e.target.value <80) {
+    if (e.target.value > 25 && e.target.value <40) {
       document.getElementById("poem2").style.opacity = '1'
     } else {
       document.getElementById("poem2").style.opacity = '0'
     }
-    if (e.target.value > 20 && e.target.value <30) {
+    if (e.target.value > 65 && e.target.value <75) {
         document.getElementById("poem4").style.opacity = '1'
       } else {
         document.getElementById("poem4").style.opacity = '0'
       } 
-    if (e.target.value > 20 && e.target.value <30) {
-    document.getElementById("background-video").style.opacity = '.3'
+    if (e.target.value > 25 && e.target.value <75) {
+    document.getElementById("background-video1").style.opacity = '.3'
   } else {
-    document.getElementById("background-video").style.opacity = '0'
+    document.getElementById("background-video1").style.opacity = '0'
+  }
+  if (e.target.value > 55 && e.target.value <101) {
+    document.getElementById("background-video2").style.opacity = '.6'
+  } else {
+    document.getElementById("background-video2").style.opacity = '0'
+  }
+  if (e.target.value > -1 && e.target.value <45) {
+    document.getElementById("background-video3").style.opacity = '.3'
+  } else {
+    document.getElementById("background-video3").style.opacity = '0'
   }
 }
   
